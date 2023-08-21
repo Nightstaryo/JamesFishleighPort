388d870a2665ab16c31620e5c59db66a74ab8f50
Nightstaryo <jamesfishleigh2000@gmail.com>
2023-08-21 12:57:46 +0100

    Experience addition

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -15,11 +15,20 @@ const Experience=() =>{
           <h1>Novella</h1>
        
           <div className="prod-dsc">Novela is a web application for uploading and reading novels. The web application allows user to browse a library of free novels created by other users. All users who sign up can upload and edit their own novels. I am developing this project to showcase my experience with the skills I have previously listed. </div>
-          
-      
-          <div className="prod-logs">LogIn Details</div>
           <div className="prod-buttons">
           <div className="prod-button">LiveSite</div>
+          <div className="prod-button">Source Code</div>
+          </div>
+          </div>
+          <div className="Project-Box">
+          
+          <h1>Disertaion Project</h1>
+       
+          <div className="prod-dsc">This it the project i built for my dissertaion. It is a web application which allows users to schedule their devices using a greedy algorithm to take advantage of tarrif price changes. </div>
+          <div className="prod-buttons">
+          <div className="prod-button"> <a href="http://unn-w19012185.newnumyspace.co.uk/project/app/" target="_blank" rel="noopener noreferrer">
+          LiveSite
+      </a></div>
           <div className="prod-button">Source Code</div>
           </div>
           </div>
